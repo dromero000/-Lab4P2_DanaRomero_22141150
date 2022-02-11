d7d1257f6a7806490dc2d24a1973387a382b1a22
dromero000 <d.romero@unitec.edu>
2022-02-11 17:03:33 -0600

    Función pelea

--- src/RomeoJulieta/Main.java
+++ src/RomeoJulieta/Main.java
@@ -6,6 +6,7 @@
 package RomeoJulieta;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Scanner;
  * @author Dana Romero
  */
 public class Main {
-    static Scanner leer = new Scanner (System.in);
+    static Scanner leer = new Scanner (System.in).useDelimiter("\n");
     static ArrayList <Familia> lista_familia = new ArrayList<Familia>();
         
     public static void main(String[] args) {
@@ -66,15 +67,15 @@ public class Main {
         lista_familia.get(1).lista_aldeanos.add(new Pacifista ("Guillermo", "Capuleto", 33, 200));
         
         //La familia en la posición 2 de Romero
-        lista_familia.add(new Familia ("López"));
-        lista_familia.get(2).lista_aldeanos.add(new Herrero ("Pablo", "López", 57, 300));
-        lista_familia.get(2).lista_aldeanos.add(new Agronomo("Manuel", "López", 36, 200));
-        lista_familia.get(2).lista_aldeanos.add(new Pacifista ("María", "López", 27, 150 ));
+        lista_familia.add(new Familia ("Lopez"));
+        lista_familia.get(2).lista_aldeanos.add(new Herrero ("Pablo", "Lopez", 57, 300));
+        lista_familia.get(2).lista_aldeanos.add(new Agronomo("Manuel", "Lopez", 36, 200));
+        lista_familia.get(2).lista_aldeanos.add(new Pacifista ("María", "Lopez", 27, 150 ));
     }
     
     public static boolean crearFamilia(){
         System.out.print("Ingrese apellido: ");
-        String apellido = leer.nextLine();
+        String apellido = leer.next();
         for (Familia fam :lista_familia){
             if (fam.linaje.equals(apellido)){
                 System.out.println("Esta familia ya existe");
@@ -86,13 +87,13 @@ public class Main {
     }
     
     public static boolean crearAldeano(){
-        System.out.println("Ingrese nombre: ");
-        String nombre=leer.nextLine();
+        System.out.print("Ingrese nombre: ");
+        String nombre=leer.next();
         System.out.print("Ingrese apellido: ");
-        String apellido = leer.nextLine();
-        System.out.println("Ingrese edad: ");
+        String apellido = leer.next();
+        System.out.print("Ingrese edad: ");
         int edad = leer.nextInt();
-        System.out.println("Ingrese puntos de vida");
+        System.out.print("Ingrese puntos de vida: ");
         int puntosVida = leer.nextInt();
         for (Familia fam :lista_familia){
             if (fam.linaje.equals(apellido)){
@@ -120,8 +121,38 @@ public class Main {
                 return true;
             }
         }
-    
+        System.out.println("La familia "+ apellido+" no existe");
        return false; 
+    }
+    
+    public static Familia buscarFamilia(String apellido){
+        for (Familia fam: lista_familia){
+            if(fam.linaje.equals(apellido)){
+                return fam;
+            }
+            
+        }
+        return null;
+    }
+    
+    public static void pelea(){
+        System.out.println("Ingrese el apellido de la familia contrincante: ");
+        String apellido = leer.next();
+        Familia contrincante = buscarFamilia(apellido);
+        Familia montesco = buscarFamilia("Montesco");
+        if (contrincante!=null && contrincante.lista_aldeanos.size()>=1){
+            System.out.println("Montesco vs. "+apellido);
+            Collections.shuffle(contrincante.lista_aldeanos);
+            Collections.shuffle(montesco.lista_aldeanos);
+            while(contrincante.lista_aldeanos.size()>0 && montesco.lista_aldeanos.size()>0){
+                int i = 0;
+                Aldeano jugador1 = contrincante.lista_aldeanos.get(i);
+                Aldeano jugador2 = montesco.lista_aldeanos.get(i);
+            }
+            
+        }
+            
+        
     }
     
 
